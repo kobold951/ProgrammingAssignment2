ed82925e70de64b5e9d576c25694baa8f6db96bd
unknown <roxas97531@hotmail.com>
2022-06-30 20:02:47 -0500

    Pushing assignment to GitHub

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Haciendo que primero revise si existe la matriz en la cache para 
+# no reprocesar y así ahorrar memoria de procesamiento.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## La primera función genera una lista para definir y determinar valores 
+# de una matriz, luego definir y determinar los valores del inverso de la matriz.
 
+makeCacheMatrix <- function(m = matrix()) {
+      invertedmatrix <- NULL
+      set <- function(matrix) {
+            m <<- matrix
+            invertedmatrix <<- NULL
+      }
+      get <- function() m
+      setinverse <- function(inverse) invertedmatrix <<- inverse
+      getinverse <- function() invertedmatrix
+      list(set = set, get = get, 
+           setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## La segunda función primero revisa si la matriz ya se calculó; si es el caso, 
+# regresa el valor del resultado en la memoria. Si no, lo determina. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m, ...) {
+      invertedmatrix <- m$getinverse()
+      
+      if (!is.null(invertedmatrix)) {
+            message("getting cached matrix")
+            return(invertedmatrix)
+      }
+      
+      data <- m$get()
+      invertedmatrix <- solve(data, ...)
+      m$setinverse(invertedmatrix)
+      invertedmatrix
 }
+
+#### Test ####
+
+#mat <- matrix(rpois(9, 5), nrow = 3, ncol = 3)
+#matr <- makeCacheMatrix(mat)
+#mat == matr$get()
+#cacheSolve(matr)
+#cacheSolve(matr) # ya regresa la leyenda "getting cached matrix"
